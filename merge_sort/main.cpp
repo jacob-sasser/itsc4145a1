a5f1178f43cf4d92e75edc4b94512086f18207bd
abc <jacobsasser23@gmail.com>
2025-02-05 13:41:12 -0500

    update main.cpp

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <vector>
 #include <iostream>
-void merge(std::vector<int>& arr, int left, int right, int mid){
-    int n1= left-mid+1;
+void merge(std::vector<int>& arr, int left, int mid, int right){
+    int n1= mid-left+1;
     int n2=right-mid;
     std::vector<int> L(arr.begin() + left, arr.begin() + left + n1);
     std::vector<int> R(arr.begin() + mid + 1, arr.begin() + mid + 1 + n2);
@@ -41,18 +41,29 @@ void mergeSort(std::vector<int>& arr,int left, int right){
 }
 
 int main(int argc, char* argv[]){
-    size_t size=atoi(argv[1]);
+  size_t size=std::stoul(argv[1]);
+  int sizes[9]={10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+    for(int j=0;j<10;j++){
+      size=sizes[j]; 
     std::vector<int> arr(size);
-    for(size_t i =0;i<size;i++){
+    srand(time(0));
+   
+    for(int i =0;i<size;i++){
         arr[i]=rand()%100;
     }
-    srand(time(0));
+    
+    std::cout<<"Array size: "<<size<<"\n";
     time_t time1=time(0);
     mergeSort(arr,0,arr.size()-1);
     time_t time2=time(0);
     time_t diff=time2-time1;
-    std::cout<<"Time taken:"<< diff <<"\n";
+
+    std::cout<<"Time taken:"<< (diff) <<"\n";
+
+    }
     return 0;
 
 
 }
+
+
