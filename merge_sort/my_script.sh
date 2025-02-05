5b39480f7437c0828e1849ab7c6310bb789f399d
abc <jacobsasser23@gmail.com>
2025-02-05 15:11:01 -0500

    Update my_script.sh

--- merge_sort/my_script.sh
+++ merge_sort/my_script.sh
@@ -3,5 +3,5 @@
 #SBATCH --partition=Centaurus
 #SBATCH --time=01:00:00
 #SBATCH --mem=32G
-$HOME/itsc4145a1/merge_sort/g++ main.cpp -o main.exe
+$HOME/itsc4145a1/merge_sort g++ main.cpp -o main.exe
 $HOME/itsc4145a1/merge_sort/main.exe 1
